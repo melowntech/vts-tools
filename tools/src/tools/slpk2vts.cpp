36849ed3f790c7a6d4bcdcab8f66d660648d29b9
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-05-30 18:51:11 +0200

    slpk2vts: WIP

---
+++ tools/src/tools/slpk2vts.cpp
@@ -0,0 +1,265 @@
+/**
+ * Copyright (c) 2017 Melown Technologies SE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * *  Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "utility/buildsys.hpp"
+#include "utility/gccversion.hpp"
+#include "utility/limits.hpp"
+
+#include "service/cmdline.hpp"
+
+#include "vts-libs/registry/po.hpp"
+#include "vts-libs/vts.hpp"
+#include "vts-libs/vts/encoder.hpp"
+#include "vts-libs/vts/opencv/navtile.hpp"
+#include "vts-libs/vts/io.hpp"
+#include "vts-libs/vts/csconvertor.hpp"
+#include "vts-libs/vts/meshopinput.hpp"
+#include "vts-libs/vts/meshop.hpp"
+#include "vts-libs/vts/math.hpp"
+#include "vts-libs/vts/opencv/atlas.hpp"
+#include "vts-libs/vts/ntgenerator.hpp"
+#include "vts-libs/tools/progress.hpp"
+
+#include "slpk/reader.hpp"
+
+#include "./tmptileset.hpp"
+#include "./repackatlas.hpp"
+
+
+namespace po = boost::program_options;
+namespace bio = boost::iostreams;
+namespace fs = boost::filesystem;
+namespace ublas = boost::numeric::ublas;
+namespace vs = vtslibs::storage;
+namespace vr = vtslibs::registry;
+namespace vts = vtslibs::vts;
+namespace vt = vtslibs::tools;
+namespace tools = vtslibs::vts::tools;
+
+namespace {
+
+struct Config {
+    std::string tilesetId;
+    std::string referenceFrame;
+    vs::CreditIds credits;
+    int textureQuality;
+    math::Size2 optimalTextureSize;
+    double ntLodPixelSize;
+    double dtmExtractionRadius;
+
+    bool forceWatertight;
+    boost::optional<vts::LodTileRange> tileExtents;
+    double clipMargin;
+    double borderClipMargin;
+    double sigmaEditCoef;
+    bool resume;
+    bool keepTmpset;
+
+    double zShift;
+
+    Config()
+        : textureQuality(85), optimalTextureSize(256, 256)
+        , ntLodPixelSize(1.0), dtmExtractionRadius(40.0)
+        , forceWatertight(false), clipMargin(1.0 / 128.)
+        , borderClipMargin(clipMargin)
+        , sigmaEditCoef(1.5), resume(false), keepTmpset(false)
+        , zShift(0.0)
+    {}
+};
+
+class Slpk2Vts : public service::Cmdline
+{
+public:
+    Slpk2Vts()
+        : service::Cmdline("slpk2vts", BUILD_TARGET_VERSION)
+        , createMode_(vts::CreateMode::failIfExists)
+    {
+    }
+
+private:
+    virtual void configuration(po::options_description &cmdline
+                               , po::options_description &config
+                               , po::positional_options_description &pd)
+        UTILITY_OVERRIDE;
+
+    virtual void configure(const po::variables_map &vars)
+        UTILITY_OVERRIDE;
+
+    virtual bool help(std::ostream &out, const std::string &what) const
+        UTILITY_OVERRIDE;
+
+    virtual int run() UTILITY_OVERRIDE;
+
+    fs::path output_;
+    fs::path input_;
+
+    vts::CreateMode createMode_;
+
+    Config config_;
+    vt::ExternalProgress::Config epConfig_;
+};
+
+void Slpk2Vts::configuration(po::options_description &cmdline
+                             , po::options_description &config
+                             , po::positional_options_description &pd)
+{
+    vr::registryConfiguration(cmdline, vr::defaultPath());
+    vr::creditsConfiguration(cmdline);
+
+    cmdline.add_options()
+        ("output", po::value(&output_)->required()
+         , "Path to output (vts) tile set.")
+        ("input", po::value(&input_)->required()
+         , "Path to input SLPK archive.")
+        ("overwrite", "Existing tile set gets overwritten if set.")
+
+        ("tilesetId", po::value(&config_.tilesetId)->required()
+         , "Output tileset ID.")
+
+        ("referenceFrame", po::value(&config_.referenceFrame)->required()
+         , "Destination reference frame. Must be different from input "
+         "tileset's referenceFrame.")
+
+        ("textureQuality", po::value(&config_.textureQuality)
+         ->default_value(config_.textureQuality)->required()
+         , "Texture quality for JPEG texture encoding (0-100).")
+
+        ("navtileLodPixelSize"
+         , po::value(&config_.ntLodPixelSize)
+         ->default_value(config_.ntLodPixelSize)->required()
+         , "Navigation data are generated at first LOD (starting from root) "
+         "where pixel size (in navigation grid) is less or "
+         "equal to this value.")
+
+        ("dtmExtraction.radius"
+         , po::value(&config_.dtmExtractionRadius)
+         ->default_value(config_.dtmExtractionRadius)->required()
+         , "Radius (in meters) of DTM extraction element (in meters).")
+
+        ("force.watertight", po::value(&config_.forceWatertight)
+         ->default_value(false)->implicit_value(true)
+         , "Enforces full coverage mask to every generated tile even "
+         "when it is holey.")
+
+        ("clipMargin", po::value(&config_.clipMargin)
+         ->default_value(config_.clipMargin)
+         , "Margin (in fraction of tile dimensions) added to tile extents in "
+         "all 4 directions.")
+
+        ("tileExtents", po::value<vts::LodTileRange>()
+         , "Optional tile extents specidied in form lod/llx,lly:urx,ury. "
+         "When set, only tiles in that range and below are added to "
+         "the output.")
+
+        ("borderClipMargin", po::value(&config_.borderClipMargin)
+         , "Margin (in fraction of tile dimensions) added to tile extents "
+         "where tile touches artificial border definied by tileExtents.")
+
+        ("tweak.optimalTextureSize", po::value(&config_.optimalTextureSize)
+         ->default_value(config_.optimalTextureSize)->required()
+         , "Size of ideal tile texture. Used to calculate fitting LOD from"
+         "mesh texel size. Do not modify.")
+
+        ("tweak.sigmaEditCoef", po::value(&config_.sigmaEditCoef)
+         ->default_value(config_.sigmaEditCoef)
+         , "Sigma editting coefficient. Meshes with best LOD difference from "
+         "mean best LOD lower than sigmaEditCoef * sigma are assigned "
+         "round(mean best LOD).")
+
+        ("resume"
+         , "Resumes interrupted encoding. There must be complete (valid) "
+         "temporary tileset inside generated tileset. Use with caution.")
+        ("keepTmpset"
+         , "Keep temporary tileset intact on exit.")
+
+        ("zShift", po::value(&config_.zShift)
+         ->default_value(config_.zShift)->required()
+         , "Manual height adjustment (value is "
+         "added to z component of all vertices).")
+
+        ;
+
+    vt::progressConfiguration(config);
+
+    pd
+        .add("input", 1)
+        .add("output", 1);
+
+    (void) config;
+}
+
+void Slpk2Vts::configure(const po::variables_map &vars)
+{
+    vr::registryConfigure(vars);
+    config_.credits = vr::creditsConfigure(vars);
+
+    createMode_ = (vars.count("overwrite")
+                   ? vts::CreateMode::overwrite
+                   : vts::CreateMode::failIfExists);
+
+    if ((config_.textureQuality < 0) || (config_.textureQuality > 100)) {
+        throw po::validation_error
+            (po::validation_error::invalid_option_value, "textureQuality");
+    }
+
+    if (vars.count("tileExtents")) {
+        config_.tileExtents = vars["tileExtents"].as<vts::LodTileRange>();
+    }
+
+    config_.resume = vars.count("resume");
+    config_.keepTmpset = vars.count("keepTmpset");
+    epConfig_ = vt::configureProgress(vars);
+}
+
+bool Slpk2Vts::help(std::ostream &out, const std::string &what) const
+{
+    if (what.empty()) {
+        out << R"RAW(slpk2vts
+usage
+    slpk2vts INPUT OUTPUT [OPTIONS]
+
+)RAW";
+    }
+    return false;
+}
+
+int Slpk2Vts::run()
+{
+    // open archive
+    slpk::Archive input(input_);
+
+    // all done
+    LOG(info4) << "All done.";
+    return EXIT_SUCCESS;
+}
+
+} // namespace
+
+int main(int argc, char *argv[])
+{
+    utility::unlimitedCoredump();
+    return Slpk2Vts()(argc, argv);
+}
