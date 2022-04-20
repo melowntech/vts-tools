33196f200f526c6f3dc2b1d4855b185b739da630
Vaclav Blazek <vaclav.blazek@citationtech.net>
2022-04-20 15:55:57 +0200

    using 64-bit tilar

--- tools/src/tools/vef2slpk.cpp
+++ tools/src/tools/vef2slpk.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Melown Technologies SE
+ * Copyright (c) 2017-2022 Melown Technologies SE
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
